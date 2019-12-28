a605b82f8b0495d5a5caa56c5476369a2ee37837
huangwl <45049572+huangweilai@users.noreply.github.com>
2019-12-28 17:22:25 +0800

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -5,24 +5,28 @@ Sat Apr 13 18:26:29 CST 2019
 This Scripts For CentOS6 ！
 !
 
-# 检查系版本
+
+# 检查用户
+check_user(){
+[[ $UID != "0" ]] ||  echo -e "exit, this script does not support the current user!" && exit 1
+}
+
+# 检查系统版本
 check_sys(){
 sys_release=`cat /etc/redhat-release | awk '{printf $1;print $4}' | cut -c1-7`
-[[ $sys_release != "CentOS7" ]] && echo -e "Exited, this script does not support the current system!" && exit 1
+[[ $sys_release != "CentOS7" ]] && echo -e "exit, this script does not support the current system!" && exit 2
 }
 
 # 备份文件
 Backup_conf(){
-cp -p /etc/selinux/config /etc/selinux/config.bak
-tar -cPf /etc/yum.repos.d/repo-bak.tar /etc/yum.repos.d/*.repo
-mkdir /etc/yum.repos.d/bak/;mv *.repo /etc/yum.repos.d/bak/
-tar -cPf /etc/sysconfig/network-scripts/ifconfig-bak.tar /etc/sysconfig/network-scripts/*
+cd /etc/selinux/ && tar -czf selinux_bak.tar.gz ./*
+mkdir /etc/yum.repos.d/repo_bak ; mv /etc/yum.repos.d/*.repo repo_bak/ ; cd /etc/yum.repos.d/ && tar -czf yum_repo_bak.tar.gz repo_bak/
+cd /etc/sysconfig/network-scripts && tar -czf network.tar.gz ./*
+cd /etc/ssh/ && tar -czf ssh_tar.gz ./*
 }
 
 # 配置ssh
 ssh_conf(){
-cp /etc/ssh/sshd_config /etc/ssh/sshd_config-bak
-cp /etc/ssh/ssh_config /etc/ssh/ssh_config-bak
 sed -i -e 's/#UseDNS yes/UseDNS no/g' -e 's/GSSAPIAuthentication yes/GSSAPIAuthentication no/g' /etc/ssh/sshd_config
 sed -i '$a StrictHostKeyChecking no' /etc/ssh/ssh_config
 /etc/rc.d/init.d/sshd restart > /dev/null 2>&1
@@ -43,9 +47,46 @@ setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 }
 
+set_datime(){
+yum -y install ntp ntpdate > /dev/null 2>&1
+ntpdate ntp.aliyun.com > /dev/null 2>&1
+hwclock --systohc > /dev/null 2>&1
+hwclock -w > /dev/null 2>&1
+}
+
+# 执行
+check_user
+check_sys
+Backup_conf
+ssh_conf
+Setup_tool
+Disable_firewall
+set_datime
+
+:<<!
+# 创建yum仓库
+make_yum(){
+rm -rf /etc/yum.repos.d/*.repo
+echo -e "[local] \nname=local \nbaseurl=file:///mnt/ \ngpgcheck=0 \nenable=1 \ngpgkey=file:///mnt/" > local.repo
+mount -t iso9660 /dev/sr0 /mnt >/dev/null 2>&1 && yum clean all >/dev/null 2>&1 && yum makecache > /dev/null 2>&1
+Setup_tool
+sleep 2
+}
+CentOS6:cat /etc/redhat-release | awk '{printf $1;print $3}' | cut -c1-7
+CentOS7:cat /etc/redhat-release | awk '{printf $1;print $4}' | cut -c1-7
+# set_PS1
+set_envps1(){
+echo 'PS1="\[\e[1;31m\]\u\[\e[1;33m\]@\H \[\e[1;36m\]\w\[\e[0m\] \\$ "' > /etc/profile.d/env.sh
+source /etc/profile.d/env.sh
+}
+
 # 获取IP
 get_ip(){
 ip add | grep "scope global" | awk '{print $NF,$2}' 
+}
+!
+
+
 }
 
 # set_PS1
